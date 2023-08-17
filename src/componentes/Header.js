5cf27e8a57ff108322ee79d22e8e77a24b640e8b
yuleysi <zulemagomez.2014@gmail.com>
2023-08-17 15:12:25 -0600

    Alternativa de para escribir componentes funcionales

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -1,15 +1,7 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 
-function Header(props) {
-
-    console.log(props)
-
-    return(
-        <Fragment>
-        <h1>{props.titulo}</h1>
-        <p>{props.descripcion}</p>
-        </Fragment>
-    );
+const Header = ({titulo}) => {
+    <h1>{titulo}</h1>
 }
-
+ 
 export default Header;

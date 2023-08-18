8b12dc2946bed74ae19d76274b4b7fbc52b01363
yuleysi <zulemagomez.2014@gmail.com>
2023-08-18 13:20:47 -0600

    Agregando el componente para la cantidad a pagar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Fragment, useState} from 'react';
 import Header from './componentes/Header';
 import Formulario from './componentes/Formulario';
+import Mensaje from './componentes/Mensaje';
 
 function App() {
   //Definir el state
   const [cantidad, guardarCantidad] = useState(0);
   const [plazo, guardarPlazo] = useState('');
+  const [total, guardarTotal] = useState(0);
 
   return (
     <Fragment>
@@ -20,7 +22,12 @@ function App() {
             guardarCantidad={guardarCantidad}
             plazo={plazo}
             guardarPlazo={guardarPlazo}
+            total={total}
+            guardarTotal={guardarTotal}
         />
+        < div className="mensajes">
+          <Mensaje/>
+        </div>
       </div>
     </Fragment>
 
